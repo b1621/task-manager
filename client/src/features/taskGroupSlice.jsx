fbb25bcf90b3b7cebaa6cc6d4e19c78de9d33f9d
b0 <bewuket1621@gmail.com>
2024-02-17 09:26:03 +0300

    create a taskgroupslice

---
+++ client/src/features/taskGroupSlice.jsx
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  taskGroups: [
+    {
+      id: "12",
+      taskGroup: "backend",
+      descritpion: "some descritpion",
+      status: "active",
+      startDate: "February 5, 2023",
+    },
+    {
+      id: "111",
+      taskGroup: "Frontend",
+      descritpion: "some descritpion",
+      status: "active",
+      startDate: "February 5, 2023",
+    },
+    {
+      id: "13",
+      taskGroup: "management",
+      descritpion: "some descritpion",
+      status: "active",
+      startDate: "February 5, 2023",
+    },
+  ],
+};
+
+const taskGroupSlice = createSlice({
+  name: taskGroup,
+  initialState,
+  reducers: {
+    addTaskGroup: (state, action) => {
+      state.taskGroups.push(action.payload);
+    },
+  },
+});
+
+export const { addTaskGroup } = taskGroupSlice.actions;
+export default taskGroupSlice.reducer;
--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskGroupSlice from "./features/taskGroupSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    taskGroup: taskGroupSlice,
+  },
 });
